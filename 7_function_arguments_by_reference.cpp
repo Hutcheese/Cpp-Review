cbd66755c27b605a4c4613abda60677ae1917676
Hutcheese <jhcunninghamv+git@gmail.com>
2025-03-01 15:13:31 -0500

    feat: initialized classes example

--- 7_function_arguments_by_reference.cpp
+++ 7_function_arguments_by_reference.cpp
@@ -10,10 +10,30 @@ void add_one_ref(int * n) {
     // n is a pointer that points to a memory-address outside the function scope
     cout << "Accessing int * n arguement..." << endl;
     cout << "      n:   "<< n << endl;
-    cout << "      &n:  " << &n << "  (&n is an address-of operator)"<< endl;
+    cout << "      &n:  " << &n << "  (&n is an address-of operator, irrelevant since n is a pointer)"<< endl;
     cout << "      *n:  " << *n << "         (*n is a dereference operator)" << endl;
     
     (*n)++;
+}
+
+typedef struct {
+    char * name;
+    int age;
+} person;
+
+void birthday(person * p);
+void birthday(person * p) {
+    p->age++;
+}
+
+void pointer_to_structure_example() {
+    person john;
+    john.name = "John";
+    john.age = 20;
+
+    printf("John's name is %s and he is %d years old\n", john.name, john.age);
+    birthday(&john);
+    printf("John's name is %s and he is %d years old\n\n", john.name, john.age);
 }
 
 void print_function_arguments_as_reference_example() {
@@ -28,11 +48,12 @@ void print_function_arguments_as_reference_example() {
     n = 0;
     add_one_ref(&n);
     cout << "   Reference -> add_one_ref(&n) output: " << n << endl;
+    cout << endl;
 }
 
 int main() {
     print_function_arguments_as_reference_example();
-
+    pointer_to_structure_example();
 
     return 0;
 }
